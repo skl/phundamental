49a7d7220f151fe5de23ac71f235dedd1fa72ca2
Stephen Lang <info@skl.me>
2012-12-15 21:05:01 +0000

    Updated install.conf

--- install.sh
+++ install.sh
@@ -8,8 +8,6 @@
 . install.conf
 
 read -p "Specify nginx version (e.g. 1.2.3): " NGINX_VERSION_STRING
-read -p "Specify PHP version (e.g. 5.4.7): " PHP_VERSION_STRING
-read -p "Specify MariaDB version (e.g. 5.5.27): " MARIADB_VERSION_STRING
 
 ${FW_INSTALL_DIR}/system-dependencies/install.sh
 ${FW_INSTALL_DIR}/nginx/install.sh ${NGINX_VERSION_STRING}
