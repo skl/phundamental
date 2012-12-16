cb4be54df40cb4ebe43c1c16621df8de7a25feff
Stephen Lang <info@skl.me>
2012-12-16 00:55:13 +0000

    ... ascii

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,9 +2,17 @@
 
 PH_INSTALL_DIR="/usr/local/src/phundamental"
 
+cat <<EOA
+     _             _                   _       _
+ ___| |_ _ _ ___ _| |___ _____ ___ ___| |_ ___| |
+| . |   | | |   | . | .'|     | -_|   |  _| .'| |
+|  _|_|_|___|_|_|___|__,|_|_|_|___|_|_|_| |__,|_|
+EOA
+echo -n '|_|'
+
 for i in `ls -1 ${PH_INSTALL_DIR}/functions.d`; do
     echo -n '.'
     . ${PH_INSTALL_DIR}/functions.d/$i
 done
 
-echo 'Bootstrap complete.'
+echo -e "Bootstrap complete \n"
