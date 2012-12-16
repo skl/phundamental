6bd33339e84f4dd6e2dcc56390e0f44fc3a2a1ee
Stephen Lang <info@skl.me>
2012-12-16 20:30:04 +0000

    Improved package manager detection by using

--- system-dependencies/install.sh
+++ system-dependencies/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "${PH_OS}" != "mac" ] ; then
-    read -p "Install build tools (make, libtool etc.)? [y/n]"
+    read -p "Install build tools (make, libtool etc.)? [y/n]: " REPLY
     [ "$REPLY" == "y" ] || echo "I would: ${PH_PACKAGE_MANAGER_BUILDTOOLS}"
 else
     echo "Build tools not required, you're on a Mac."
