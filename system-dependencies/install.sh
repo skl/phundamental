23064ea630e3b693bf06a1e37283418c8142f303
Stephen Lang <info@skl.me>
2012-12-16 20:32:59 +0000

    Bugfixed buildtools install line

--- system-dependencies/install.sh
+++ system-dependencies/install.sh
@@ -2,7 +2,7 @@
 
 if [ "${PH_OS}" != "mac" ] ; then
     read -p "Install build tools (make, libtool etc.)? [y/n]: " REPLY
-    [ "$REPLY" == "y" ] || echo "I would: ${PH_PACKAGE_MANAGER_BUILDTOOLS}"
+    [ "$REPLY" == "y" ] && echo "I would: ${PH_PACKAGE_MANAGER_BUILDTOOLS}"
 else
     echo "Build tools not required, you're on a Mac."
 fi
