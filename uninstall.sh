ca2e23dc0f34188a29fcb983b625a8ff2830e02b
Stephen Lang <stephen.lang@symphony-group.co.uk>
2012-12-27 11:00:56 +0000

    Added uninstaller

---
+++ uninstall.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#####################################################
+#      _             _                   _       _  #
+#  ___| |_ _ _ ___ _| |___ _____ ___ ___| |_ ___| | #
+# | . |   | | |   | . | .'|     | -_|   |  _| .'| | #
+# |  _|_|_|___|_|_|___|__,|_|_|_|___|_|_|_| |__,|_| #
+# |_|                                               #
+#                                                   #
+#####################################################
+
+if [ 0 -ne `id -u` ]; then
+    echo 'You must be root to run this script!'
+    exit
+fi
+
+WHEREAMI=`dirname $0`
+. ${WHEREAMI}/conf.d/phundamental.conf
+
+. ${PH_INSTALL_DIR}/bootstrap.sh
+
+for i in `ls -1 ${PH_INSTALL_DIR}/modules`; do
+    UNINSTALLER=${PH_INSTALL_DIR}/modules/$i/uninstall.sh
+
+    # Uninstall module if there is an executable uninstall.sh
+    if [ -x $UNINSTALLER ]; then
+        echo ""
+        read -p "Would you like to uninstall '$i'? [y/n] " REPLY
+        [ "y" == $REPLY ] && . ${UNINSTALLER}
+    fi
+done
