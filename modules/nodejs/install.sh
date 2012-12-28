0e583d0e0547266093e91838dbbcd62de78c88b2
Stephen Lang <stephen.lang@symphony-group.co.uk>
2012-12-28 13:20:25 +0000

    Notify windows user of node installer

--- modules/nodejs/install.sh
+++ modules/nodejs/install.sh
@@ -7,51 +7,57 @@
 NODEJS_VERSION_STRING=$1
 [ -z "$1" ] && read -p "Specify node.js version (e.g. 0.8.16): " NODEJS_VERSION_STRING
 
-read -p "Install node.js dependencies? [y/n]: " REPLY
-[ "$REPLY" == "y" ] && ph_install_packages openssl
+if [ "${PH_OS}" == "windows" ]; then
+    echo "You need to download the installer:"
+    echo "http://nodejs.org/dist/v${NODEJS_VERSION_STRING}/node-v${NODEJS_VERSION_STRING}-x86.msi"
 
-read -p "Overwrite existing symlinks? [y/n]: " REPLY
-[ "$REPLY" == "y" ] && NODEJS_OVERWRITE_SYMLINKS=true || NODEJS_OVERWRITE_SYMLINKS=false
+else
+    read -p "Install node.js dependencies? [y/n]: " REPLY
+    [ "$REPLY" == "y" ] && ph_install_packages openssl python
 
-ph_mkdirs \
-    /usr/local/src \
-    /etc/nodejs-${NOEJS_VERSION_STRING} \
-    /var/log/nodejs-${NODEJS_VERSION_STRING}
+    read -p "Overwrite existing symlinks? [y/n]: " REPLY
+    [ "$REPLY" == "y" ] && NODEJS_OVERWRITE_SYMLINKS=true || NODEJS_OVERWRITE_SYMLINKS=false
 
-cd /usr/local/src
+    ph_mkdirs \
+        /usr/local/src \
+        /etc/nodejs-${NOEJS_VERSION_STRING} \
+        /var/log/nodejs-${NODEJS_VERSION_STRING}
 
-if [ ! -f node-v${NODEJS_VERSION_STRING}.tar.gz ]; then
-    wget http://nodejs.org/dist/v${NODEJS_VERSION_STRING}/node-v${NODEJS_VERSION_STRING}.tar.gz
+    cd /usr/local/src
 
     if [ ! -f node-v${NODEJS_VERSION_STRING}.tar.gz ]; then
-        echo "node.js source download failed!"
-        return 1
+        wget http://nodejs.org/dist/v${NODEJS_VERSION_STRING}/node-v${NODEJS_VERSION_STRING}.tar.gz
+
+        if [ ! -f node-v${NODEJS_VERSION_STRING}.tar.gz ]; then
+            echo "node.js source download failed!"
+            return 1
+        fi
     fi
+
+    tar xzf node-v${NODEJS_VERSION_STRING}.tar.gz
+    cd node-v${NODEJS_VERSION_STRING}
+
+    CONFIGURE_ARGS=("--prefix=/usr/local/nodejs-${NODEJS_VERSION_STRING}");
+
+    if [[ "${PH_PACKAGE_MANAGER}" == "brew" ]]; then
+        # Add homebrew include directories
+        CONFIGURE_ARGS=("${CONFIGURE_ARGS[@]}" \
+            "--with-cc-opt=-I/usr/local/include" \
+            "--with-ld-opt=-L/usr/local/lib")
+    fi
+
+    ./configure ${CONFIGURE_ARGS[@]} && make -j ${PH_NUM_CPUS} && make install
+
+    if $NODEJS_OVERWRITE_SYMLINKS ; then
+        ph_symlink /etc/nodejs-${NODEJS_VERSION_STRING} /etc/nodejs
+        ph_symlink /usr/local/nodejs-${NODEJS_VERSION_STRING} /usr/local/nodejs
+        ph_symlink /usr/local/nodejs-${NODEJS_VERSION_STRING}/logs /var/log/nodejs
+        ph_symlink /usr/local/nodejs/bin/node /usr/local/bin/node
+        ph_symlink /usr/local/nodejs/bin/node-waf /usr/local/bin/node-waf
+        ph_symlink /usr/local/nodejs/bin/npm /usr/local/bin/npm
+    fi
+
+
+    echo ""
+    echo "node.js ${NODEJS_VERSION_STRING} has been installed!"
 fi
-
-tar xzf node-v${NODEJS_VERSION_STRING}.tar.gz
-cd node-v${NODEJS_VERSION_STRING}
-
-CONFIGURE_ARGS=("--prefix=/usr/local/nodejs-${NODEJS_VERSION_STRING}");
-
-if [[ "${PH_PACKAGE_MANAGER}" == "brew" ]]; then
-    # Add homebrew include directories
-    CONFIGURE_ARGS=("${CONFIGURE_ARGS[@]}" \
-        "--with-cc-opt=-I/usr/local/include" \
-        "--with-ld-opt=-L/usr/local/lib")
-fi
-
-./configure ${CONFIGURE_ARGS[@]} && make -j ${PH_NUM_CPUS} && make install
-
-if $NODEJS_OVERWRITE_SYMLINKS ; then
-    ph_symlink /etc/nodejs-${NODEJS_VERSION_STRING} /etc/nodejs
-    ph_symlink /usr/local/nodejs-${NODEJS_VERSION_STRING} /usr/local/nodejs
-    ph_symlink /usr/local/nodejs-${NODEJS_VERSION_STRING}/logs /var/log/nodejs
-    ph_symlink /usr/local/nodejs/bin/node /usr/local/bin/node
-    ph_symlink /usr/local/nodejs/bin/node-waf /usr/local/bin/node-waf
-    ph_symlink /usr/local/nodejs/bin/npm /usr/local/bin/npm
-fi
-
-
-echo ""
-echo "node.js ${NODEJS_VERSION_STRING} has been installed!"
