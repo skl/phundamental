4b5bb44e5c5d47e5a846b7564c68599cbf6c080d
Stephen Lang <stephen.lang@symphony-group.co.uk>
2012-12-28 12:13:40 +0000

    Added zlib to nginx dependencies

--- modules/nginx/install.sh
+++ modules/nginx/install.sh
@@ -8,7 +8,7 @@ NGINX_VERSION_STRING=$1
 [ -z "$1" ] && read -p "Specify nginx version (e.g. 1.2.6): " NGINX_VERSION_STRING
 
 read -p "Install nginx dependencies? [y/n]: " REPLY
-[ "$REPLY" == "y" ] && ph_install_packages pcre openssl
+[ "$REPLY" == "y" ] && ph_install_packages pcre openssl zlib
 
 read -p "Overwrite existing symlinks? [y/n]: " REPLY
 [ "$REPLY" == "y" ] && NGINX_OVERWRITE_SYMLINKS=true || NGINX_OVERWRITE_SYMLINKS=false
