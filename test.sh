28d21e53aa16cf7879d1ee46d137b28d43f1a36f
Stephen Lang <info@skl.me>
2013-01-10 20:59:56 +0000

    Added .travis.yml

--- test.sh
+++ test.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-for i in `find . -name "*.sh"`; do bash -n ${i} && echo "SYNTAX OK: ${i}"; done
+
+# 0 = PASS
+# 1 = FAIL
+PH_TEST_FAILURE=0
+
+# Run syntax check
+for i in `find . -name "*.sh"`; do
+    bash -n ${i} && echo "SYNTAX OK: ${i}" || PH_TEST_FAILURE=1
+done
+
+exit ${PH_TEST_FAILURE}
