d4b88b96f343c9961815eded23ddad02390a39cc
Stephen Lang <stephen.lang@symphony-group.co.uk>
2013-01-15 13:35:22 +0000

    Added optional module arguments to top-level installer

--- install.sh
+++ install.sh
@@ -10,20 +10,43 @@
 
 # Absolute path to current script http://stackoverflow.com/a/246128
 PH_INSTALL_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Check arguments are valid modules if they exists
+for i in `echo $@ | tr " " "\n"`; do
+    [ ! -d "${PH_INSTALL_DIR}/modules/$i" ] && { echo "[phundamental/installer] Module '$i' does not exist!" && exit 1; }
+done
+
 . ${PH_INSTALL_DIR}/bootstrap.sh
 
-for i in `ls -1 ${PH_INSTALL_DIR}/modules`; do
-    INSTALLER=${PH_INSTALL_DIR}/modules/$i/install.sh
+function ph_install_module() {
+    local MODULE=$1
+    local RESULT_MSG='with an unknown status'
+    local INSTALLER=${PH_INSTALL_DIR}/modules/${MODULE}/install.sh
 
-    # Install module if there is an executable install.sh
-    if [ -x $INSTALLER ]; then
-        echo ''
-        read -p "[phundamental/installer] Would you like to install '$i'? [y/n] " REPLY
-        if [ "y" == $REPLY ]; then
-            RESULT='with an unknown status'
-            . ${INSTALLER} && RESULT='successfully' || RESULT='unsuccessfully'
-            echo "[phundamental/$i] installation script finished ${RESULT}."
+    . ${INSTALLER} && RESULT='successfully' || RESULT='unsuccessfully'
+    echo "[phundamental/${MODULE}] installation script finished ${RESULT}."
+
+    if [[ "${RESULT}" == "successfully" ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+for i in `ls -1 ${PH_INSTALL_DIR}/modules`; do
+    if [ -x ${PH_INSTALL_DIR}/modules/$i/install.sh ]; then
+        # Allow specific modules to be installed from an argument list
+        if [ $# -gt 0 ]; then
+            echo $@ | tr " " "\n" | grep "^${i}$" >/dev/null && ph_install_module $i
+
+        # Ask use if they'd like to install the module
+        else
+            echo ''
+            read -p "[phundamental/installer] Would you like to install '$i'? [y/n] " REPLY
+            [ "y" == $REPLY ] && ph_install_module $i
         fi
+    else
+        echo "[phundamental/$i] Installer is not executable, skipping..."
     fi
 done
 
