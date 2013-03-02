bdd040f34ef65d212c5fc608f6da3fdeb1d6a7f1
Stephen Lang <info@skl.me>
2013-03-02 17:43:14 +0000

    Removed plz from test.sh

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@
 PH_TEST_FAILURE=0
 
 # Run syntax check
-for i in plz install uninstall `find . -name "*.sh"`; do
+for i in install uninstall `find . -name "*.sh"`; do
     bash -n ${i} && echo "SYNTAX OK: ${i}" || PH_TEST_FAILURE=1
 done
 
