382a5629b685151c45126a36d58544c8e3aac4f9
Stephen Lang <info@skl.me>
2014-01-24 22:32:46 +0000

    [nginx] ph_module_instal_nginx(), option parsing

--- lib/1.d/user_management.sh
+++ lib/1.d/user_management.sh
@@ -7,7 +7,7 @@ function ph_createuser_mac() {
     local NEW_USER_ID=`expr ${MAX_USER_ID} + 1`
 
     # Return early if user exists
-    dscl . -read /Users/${USERNAME} >/dev/null 2>&1 && return 0
+    dscl . -read /Users/${USERNAME} &>/dev/null && return 0
 
     dscl . -create /Users/${USERNAME}
     dscl . -create /Users/${USERNAME} UniqueID "${NEW_USER_ID}"
@@ -30,7 +30,7 @@ function ph_creategroup_mac() {
     local NEW_GROUP_ID=`expr ${MAX_GROUP_ID} + 1`
 
     # Return early if group exists
-    dscl . -read /Groups/${GROUPNAME} >/dev/null 2>&1 && return 0
+    dscl . -read /Groups/${GROUPNAME} &>/dev/null && return 0
 
     dscl . -create /Groups/${GROUPNAME}
     dscl . -create /Groups/${GROUPNAME} PrimaryGroupID ${NEW_GROUP_ID}
@@ -51,7 +51,7 @@ function ph_assigngroup_mac() {
     local USERNAME=$2
 
     # Return early if membership already exists
-    dscl . -read /Groups/${GROUPNAME} | grep ${USERNAME} >/dev/null && return 0
+    dscl . -read /Groups/${GROUPNAME} | grep ${USERNAME} &>/dev/null return 0
 
     dscl . -append /Groups/${GROUPNAME} GroupMembership ${USERNAME}
 
@@ -62,11 +62,11 @@ function ph_createuser_linux() {
     local USERNAME=$1
 
     # Return early if user already exists
-    id -u ${USERNAME} 2>/dev/null && return 0
+    id -u ${USERNAME} &>/dev/null && return 0
 
     # Create system user, if failed then create user and assign to group with
     # same name.
-    useradd -r ${USERNAME} 2>/dev/null \
+    useradd -r ${USERNAME} &>/dev/null \
         || useradd -rg ${USERNAME} ${USERNAME} \
         || { echo "ph_createuser_linux() failed to add user ${USERNAME}!"; return 1; }
 
@@ -83,7 +83,7 @@ function ph_creategroup_linux() {
     local GROUPNAME=$1
 
     # Return early if group already exists
-    cat /etc/group | grep "^${GROUPNAME}" >/dev/null && return 0
+    cat /etc/group | grep "^${GROUPNAME}" &>/dev/null && return 0
 
     groupadd -r ${GROUPNAME} && return 0 || return 1
 }
@@ -99,7 +99,7 @@ function ph_assigngroup_linux() {
     local USERNAME=$2
 
     # Return early if membership already exists
-    cat /etc/group | grep "^${GROUPNAME}" | cut -d: -f4 | grep ${USERNAME} >/dev/null && return 0
+    cat /etc/group | grep "^${GROUPNAME}" | cut -d: -f4 | grep ${USERNAME} &>/dev/null && return 0
 
     usermod -G ${GROUPNAME} ${USERNAME} && return 0 || return 1
 }
--- modules/nginx/install.sh
+++ modules/nginx/install.sh
@@ -8,163 +8,221 @@ PH_NGINX_INSTALL_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PH_INSTALL_DIR="$( cd "${PH_NGINX_INSTALL_DIR}" && cd ../../ && pwd )"
 . ${PH_INSTALL_DIR}/bootstrap.sh
 
-if ph_is_installed nginx ; then
-    echo "nginx is already installed!"
-    ls -l `which nginx` | awk '{print $9 $10 $11}'
-    nginx -v
+function ph_module_install_nginx()
+{
+    [ $# -gt 1 ] && NGINX_INTERACTIVE=false || NGINX_INTERACTIVE=true
 
-    if ! ph_ask_yesno "Do you wish to continue with the nginx installation?"; then
-        return 1 || exit 1
+    for arg in "$@"; do
+        case $arg in
+        --version=*)
+            NGINX_VERSION_STRING="${arg#*=}"
+            shift
+            ;;
+
+        --prefix=*)
+            NGINX_PREFIX="${arg#*=}"
+            shift
+            ;;
+
+        --user=*)
+            NGINX_USER="${arg#*=}"
+            shift
+            ;;
+
+        --group=*)
+            NGINX_GROUP="${arg#*=}"
+            shift
+            ;;
+
+        --overwrite-symlinks=*)
+            NGINX_OVERWRITE_SYMLINKS="${arg#*=}"
+            if [[ "yes" == "${NGINX_OVERWRITE_SYMLINKS}" ]]; then
+                NGINX_OVERWRITE_SYMLINKS=true
+            else
+                NGINX_OVERWRITE_SYMLINKS=false
+            fi
+            shift
+            ;;
+
+        *)
+            echo "Unknown option $arg"
+            return 1
+            ;;
+        esac
+    done
+
+    if ${NGINX_INTERACTIVE}; then
+        if ph_is_installed nginx ; then
+            echo "nginx is already installed!"
+            ls -l `which nginx` | awk '{print $9 $10 $11}'
+            nginx -v
+
+            if ! ph_ask_yesno "Do you wish to continue with the nginx installation?"; then
+                return 1
+            fi
+        fi
+
+        read -p "Specify nginx version [1.4.4]: " NGINX_VERSION_STRING
+        [ -z ${NGINX_VERSION_STRING} ] && NGINX_VERSION_STRING="1.4.4"
     fi
-fi
 
-read -p "Specify nginx version [1.4.4]: " NGINX_VERSION_STRING
-[ -z ${NGINX_VERSION_STRING} ] && NGINX_VERSION_STRING="1.4.4"
+    NGINX_VERSION_INTEGER=`echo ${NGINX_VERSION_STRING} | tr -d '.' | cut -c1-3`
+    NGINX_VERSION_INTEGER_FULL=`echo ${NGINX_VERSION_STRING} | tr -d '.'`
 
-NGINX_VERSION_INTEGER=`echo ${NGINX_VERSION_STRING} | tr -d '.' | cut -c1-3`
-NGINX_VERSION_INTEGER_FULL=`echo ${NGINX_VERSION_STRING} | tr -d '.'`
+    NGINX_VERSION_MAJOR=`echo ${NGINX_VERSION_STRING} | cut -d. -f1`
+    NGINX_VERSION_MINOR=`echo ${NGINX_VERSION_STRING} | cut -d. -f2`
+    NGINX_VERSION_RELEASE=`echo ${NGINX_VERSION_STRING} | cut -d. -f3`
 
-NGINX_VERSION_MAJOR=`echo ${NGINX_VERSION_STRING} | cut -d. -f1`
-NGINX_VERSION_MINOR=`echo ${NGINX_VERSION_STRING} | cut -d. -f2`
-NGINX_VERSION_RELEASE=`echo ${NGINX_VERSION_STRING} | cut -d. -f3`
+    if ${NGINX_INTERACTIVE}; then
+        read -p "Specify installation directory [/usr/local/nginx-${NGINX_VERSION_MAJOR}.${NGINX_VERSION_MINOR}]: " NGINX_PREFIX
+        [ -z ${NGINX_PREFIX} ] && NGINX_PREFIX="/usr/local/nginx-${NGINX_VERSION_MAJOR}.${NGINX_VERSION_MINOR}"
 
-read -p "Specify installation directory [/usr/local/nginx-${NGINX_VERSION_MAJOR}.${NGINX_VERSION_MINOR}]: " NGINX_PREFIX
-[ -z ${NGINX_PREFIX} ] && NGINX_PREFIX="/usr/local/nginx-${NGINX_VERSION_MAJOR}.${NGINX_VERSION_MINOR}"
+        case "${PH_OS}" in \
+        "linux")
+            SUGGESTED_USER="www-data"
+            ;;
 
-case "${PH_OS}" in \
-"linux")
-    SUGGESTED_USER="www-data"
-    ;;
+        "mac")
+            SUGGESTED_USER="_www"
+            ;;
+        esac
 
-"mac")
-    SUGGESTED_USER="_www"
-    ;;
-esac
+        read -p "Specify nginx user [${SUGGESTED_USER}]: " NGINX_USER
+        [ -z ${NGINX_USER} ] && NGINX_USER="${SUGGESTED_USER}"
 
-read -p "Specify nginx user [${SUGGESTED_USER}]: " NGINX_USER
-[ -z ${NGINX_USER} ] && NGINX_USER="${SUGGESTED_USER}"
+        read -p "Specify nginx group [${SUGGESTED_USER}]: " NGINX_GROUP
+        [ -z ${NGINX_GROUP} ] && NGINX_GROUP="${SUGGESTED_USER}"
+    fi
 
-read -p "Specify nginx group [${SUGGESTED_USER}]: " NGINX_GROUP
-[ -z ${NGINX_GROUP} ] && NGINX_GROUP="${SUGGESTED_USER}"
+    if ${NGINX_INTERACTIVE}; then
+        if ph_ask_yesno "Should I create the user and group for you?"; then
+            ph_creategroup ${NGINX_GROUP}
+            ph_createuser ${NGINX_USER}
+            ph_assigngroup ${NGINX_GROUP} ${NGINX_USER}
+        fi
+    else
+        ph_creategroup ${NGINX_GROUP}
+        ph_createuser ${NGINX_USER}
+        ph_assigngroup ${NGINX_GROUP} ${NGINX_USER}
+    fi
 
-if ph_ask_yesno "Should I create the user and group for you?"; then
-    ph_creategroup ${NGINX_GROUP}
-    ph_createuser ${NGINX_USER}
-    ph_assigngroup ${NGINX_GROUP} ${NGINX_USER}
-fi
+    ph_install_packages\
+        gcc\
+        make\
+        openssl\
+        pcre\
+        wget\
+        zlib
 
-ph_install_packages\
-    gcc\
-    make\
-    openssl\
-    pcre\
-    wget\
-    zlib
+    if ${NGINX_INTERACTIVE}; then
+        if ph_ask_yesno "Overwrite existing symlinks in /usr/local?"; then
+            NGINX_OVERWRITE_SYMLINKS=true
+        else
+            NGINX_OVERWRITE_SYMLINKS=false
+        fi
+    fi
 
-if ph_ask_yesno "Overwrite existing symlinks in /usr/local?"; then
-    NGINX_OVERWRITE_SYMLINKS=true
-else
-    NGINX_OVERWRITE_SYMLINKS=false
-fi
+    ph_mkdirs \
+        /usr/local/src \
+        /etc/nginx-${NGINX_VERSION_STRING} \
+        /var/log/nginx-${NGINX_VERSION_STRING} \
+        /etc/nginx-${NGINX_VERSION_STRING}/global \
+        /etc/nginx-${NGINX_VERSION_STRING}/sites-available \
+        /etc/nginx-${NGINX_VERSION_STRING}/sites-enabled \
+        /var/www/localhost/public
 
-ph_mkdirs \
-    /usr/local/src \
-    /etc/nginx-${NGINX_VERSION_STRING} \
-    /var/log/nginx-${NGINX_VERSION_STRING} \
-    /etc/nginx-${NGINX_VERSION_STRING}/global \
-    /etc/nginx-${NGINX_VERSION_STRING}/sites-available \
-    /etc/nginx-${NGINX_VERSION_STRING}/sites-enabled \
-    /var/www/localhost/public
+    ph_cd_archive tar xzf nginx-${NGINX_VERSION_STRING} .tar.gz \
+        http://nginx.org/download/nginx-${NGINX_VERSION_STRING}.tar.gz
 
-ph_cd_archive tar xzf nginx-${NGINX_VERSION_STRING} .tar.gz \
-    http://nginx.org/download/nginx-${NGINX_VERSION_STRING}.tar.gz
+    CONFIGURE_ARGS=("--prefix=${NGINX_PREFIX}"
+        "--pid-path=${NGINX_PREFIX}/logs/nginx.pid"
+        "--error-log-path=${NGINX_PREFIX}/logs/error.log"
+        "--http-log-path=${NGINX_PREFIX}/logs/access.log"
+        "--conf-path=/etc/nginx-${NGINX_VERSION_STRING}/nginx.conf"
+        "--with-pcre"
+        "--with-http_ssl_module"
+        "--with-http_realip_module");
 
-CONFIGURE_ARGS=("--prefix=${NGINX_PREFIX}"
-    "--pid-path=${NGINX_PREFIX}/logs/nginx.pid"
-    "--error-log-path=${NGINX_PREFIX}/logs/error.log"
-    "--http-log-path=${NGINX_PREFIX}/logs/access.log"
-    "--conf-path=/etc/nginx-${NGINX_VERSION_STRING}/nginx.conf"
-    "--with-pcre"
-    "--with-http_ssl_module"
-    "--with-http_realip_module");
+    if [[ "${PH_PACKAGE_MANAGER}" == "brew" ]]; then
+        # Add homebrew include directories
+        CONFIGURE_ARGS=(${CONFIGURE_ARGS[@]}
+            "--with-cc-opt=-I/usr/local/include"
+            "--with-ld-opt=-L/usr/local/lib")
+    fi
 
-if [[ "${PH_PACKAGE_MANAGER}" == "brew" ]]; then
-    # Add homebrew include directories
-    CONFIGURE_ARGS=(${CONFIGURE_ARGS[@]}
-        "--with-cc-opt=-I/usr/local/include"
-        "--with-ld-opt=-L/usr/local/lib")
-fi
+    ph_autobuild "`pwd`" ${CONFIGURE_ARGS[@]} || return 1
 
-ph_autobuild "`pwd`" ${CONFIGURE_ARGS[@]} || return 1
+    ph_cp_inject ${PH_INSTALL_DIR}/modules/nginx/nginx.conf /etc/nginx-${NGINX_VERSION_STRING}/nginx.conf\
+        "##NGINX_USER##" "${NGINX_USER}"
+    ph_search_and_replace "##NGINX_GROUP##" "${NGINX_GROUP}" /etc/nginx-${NGINX_VERSION_STRING}/nginx.conf
 
-ph_cp_inject ${PH_INSTALL_DIR}/modules/nginx/nginx.conf /etc/nginx-${NGINX_VERSION_STRING}/nginx.conf\
-    "##NGINX_USER##" "${NGINX_USER}"
-ph_search_and_replace "##NGINX_GROUP##" "${NGINX_GROUP}" /etc/nginx-${NGINX_VERSION_STRING}/nginx.conf
+    cp ${PH_INSTALL_DIR}/modules/nginx/restrictions.conf /etc/nginx-${NGINX_VERSION_STRING}/global/restrictions.conf
+    cp ${PH_INSTALL_DIR}/modules/nginx/localhost.conf /etc/nginx-${NGINX_VERSION_STRING}/sites-available/localhost
+    cp ${PH_INSTALL_DIR}/modules/nginx/000-catchall.conf /etc/nginx-${NGINX_VERSION_STRING}/sites-available/000-catchall
 
-cp ${PH_INSTALL_DIR}/modules/nginx/restrictions.conf /etc/nginx-${NGINX_VERSION_STRING}/global/restrictions.conf
-cp ${PH_INSTALL_DIR}/modules/nginx/localhost.conf /etc/nginx-${NGINX_VERSION_STRING}/sites-available/localhost
-cp ${PH_INSTALL_DIR}/modules/nginx/000-catchall.conf /etc/nginx-${NGINX_VERSION_STRING}/sites-available/000-catchall
+    ph_cp_inject ${PH_INSTALL_DIR}/modules/nginx/index.html /var/www/localhost/public/index.html\
+        "##NGINX_VERSION_STRING##" "${NGINX_VERSION_STRING}"
 
-ph_cp_inject ${PH_INSTALL_DIR}/modules/nginx/index.html /var/www/localhost/public/index.html\
-    "##NGINX_VERSION_STRING##" "${NGINX_VERSION_STRING}"
+    # Patch nginx config files for windows
+    if [ "${PH_OS}" == "windows" ]; then
+        ph_search_and_replace "^user" "#user" /etc/nginx-${NGINX_VERSION_STRING}/nginx.conf
+        ph_search_and_replace "worker_connections  1024" "worker_connections  64" /etc/nginx-${NGINX_VERSION_STRING}/nginx.conf
+    fi
 
-# Patch nginx config files for windows
-if [ "${PH_OS}" == "windows" ]; then
-    ph_search_and_replace "^user" "#user" /etc/nginx-${NGINX_VERSION_STRING}/nginx.conf
-    ph_search_and_replace "worker_connections  1024" "worker_connections  64" /etc/nginx-${NGINX_VERSION_STRING}/nginx.conf
-fi
+    ph_symlink /etc/nginx-${NGINX_VERSION_STRING} /etc/nginx ${NGINX_OVERWRITE_SYMLINKS}
+    ph_symlink ${NGINX_PREFIX} /usr/local/nginx ${NGINX_OVERWRITE_SYMLINKS}
+    ph_symlink ${NGINX_PREFIX}/logs /var/log/nginx ${NGINX_OVERWRITE_SYMLINKS}
+    ph_symlink ${NGINX_PREFIX}/sbin/nginx /usr/local/bin/nginx ${NGINX_OVERWRITE_SYMLINKS}
+    ph_symlink /etc/nginx-${NGINX_VERSION_STRING}/sites-available/localhost /etc/nginx-${NGINX_VERSION_STRING}/sites-enabled/localhost ${NGINX_OVERWRITE_SYMLINKS}
+    ph_symlink /etc/nginx-${NGINX_VERSION_STRING}/sites-available/000-catchall /etc/nginx-${NGINX_VERSION_STRING}/sites-enabled/000-catchall ${NGINX_OVERWRITE_SYMLINKS}
 
-ph_symlink /etc/nginx-${NGINX_VERSION_STRING} /etc/nginx ${NGINX_OVERWRITE_SYMLINKS}
-ph_symlink ${NGINX_PREFIX} /usr/local/nginx ${NGINX_OVERWRITE_SYMLINKS}
-ph_symlink ${NGINX_PREFIX}/logs /var/log/nginx ${NGINX_OVERWRITE_SYMLINKS}
-ph_symlink ${NGINX_PREFIX}/sbin/nginx /usr/local/bin/nginx ${NGINX_OVERWRITE_SYMLINKS}
-ph_symlink /etc/nginx-${NGINX_VERSION_STRING}/sites-available/localhost /etc/nginx-${NGINX_VERSION_STRING}/sites-enabled/localhost ${NGINX_OVERWRITE_SYMLINKS}
-ph_symlink /etc/nginx-${NGINX_VERSION_STRING}/sites-available/000-catchall /etc/nginx-${NGINX_VERSION_STRING}/sites-enabled/000-catchall ${NGINX_OVERWRITE_SYMLINKS}
+    case "${PH_OS}" in \
+    "linux")
+        case "${PH_OS_FLAVOUR}" in \
+        "arch")
+            ph_cp_inject ${PH_INSTALL_DIR}/modules/nginx/nginx.in /etc/rc.d/nginx-${NGINX_VERSION_STRING} \
+                "##NGINX_PREFIX##" "${NGINX_PREFIX}"
 
-case "${PH_OS}" in \
-"linux")
-    case "${PH_OS_FLAVOUR}" in \
-    "arch")
-        ph_cp_inject ${PH_INSTALL_DIR}/modules/nginx/nginx.in /etc/rc.d/nginx-${NGINX_VERSION_STRING} \
-            "##NGINX_PREFIX##" "${NGINX_PREFIX}"
+            /etc/rc.d/nginx-${NGINX_VERSION_STRING} start
+            ;;
 
-        /etc/rc.d/nginx-${NGINX_VERSION_STRING} start
+        "suse")
+            ph_cp_inject ${PH_INSTALL_DIR}/modules/nginx/nginx.in /etc/init.d/nginx-${NGINX_VERSION_STRING} \
+                "##NGINX_PREFIX##" "${NGINX_PREFIX}"
+
+            chkconfig nginx-${NGINX_VERSION_STRING} on
+            /etc/init.d/nginx-${NGINX_VERSION_STRING} start
+            ;;
+
+        *)
+            ph_cp_inject ${PH_INSTALL_DIR}/modules/nginx/nginx.in /etc/init.d/nginx-${NGINX_VERSION_STRING} \
+                "##NGINX_PREFIX##" "${NGINX_PREFIX}"
+
+            /etc/init.d/nginx-${NGINX_VERSION_STRING} start
+            update-rc.d nginx-${NGINX_VERSION_STRING} defaults
+            ;;
+        esac
         ;;
 
-    "suse")
-        ph_cp_inject ${PH_INSTALL_DIR}/modules/nginx/nginx.in /etc/init.d/nginx-${NGINX_VERSION_STRING} \
-            "##NGINX_PREFIX##" "${NGINX_PREFIX}"
+    "mac")
+        ph_cp_inject ${PH_INSTALL_DIR}/modules/nginx/org.nginx.nginx.plist /Library/LaunchAgents/org.nginx.nginx.plist \
+            "##NGINX_VERSION_STRING##" "${NGINX_VERSION_STRING}"
 
-        chkconfig nginx-${NGINX_VERSION_STRING} on
-        /etc/init.d/nginx-${NGINX_VERSION_STRING} start
+        chown root:wheel /Library/LaunchAgents/org.nginx.nginx.plist
+        launchctl load -w /Library/LaunchAgents/org.nginx.nginx.plist
         ;;
 
     *)
-        ph_cp_inject ${PH_INSTALL_DIR}/modules/nginx/nginx.in /etc/init.d/nginx-${NGINX_VERSION_STRING} \
-            "##NGINX_PREFIX##" "${NGINX_PREFIX}"
-
-        /etc/init.d/nginx-${NGINX_VERSION_STRING} start
-        update-rc.d nginx-${NGINX_VERSION_STRING} defaults
+        echo "nginx startup script not implemented for this OS... starting manually"
+        ${NGINX_PREFIX}/sbin/nginx
         ;;
     esac
-    ;;
 
-"mac")
-    ph_cp_inject ${PH_INSTALL_DIR}/modules/nginx/org.nginx.nginx.plist /Library/LaunchAgents/org.nginx.nginx.plist \
-        "##NGINX_VERSION_STRING##" "${NGINX_VERSION_STRING}"
+    echo -n "Deleting source files... "
+    rm -rf /usr/local/src/nginx-${NGINX_VERSION_STRING}
 
-    chown root:wheel /Library/LaunchAgents/org.nginx.nginx.plist
-    launchctl load -w /Library/LaunchAgents/org.nginx.nginx.plist
-    ;;
+    echo "Complete."
+    return 0
+}
 
-*)
-    echo "nginx startup script not implemented for this OS... starting manually"
-    ${NGINX_PREFIX}/sbin/nginx
-    ;;
-esac
-
-echo -n "Deleting source files... "
-rm -rf /usr/local/src/nginx-${NGINX_VERSION_STRING}
-
-echo "Complete."
-return 0 || exit 0
+ph_module_install_nginx $@ || exit 0
